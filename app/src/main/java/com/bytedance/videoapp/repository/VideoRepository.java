43e7ad542332e6f75b03c54922ef32fc44a4fda3
Yulong <yinyulong4@gmail.com>
2025-12-12 01:40:45 +0800

    居中tab

--- app/src/main/java/com/bytedance/videoapp/repository/VideoRepository.java
+++ app/src/main/java/com/bytedance/videoapp/repository/VideoRepository.java
@@ -18,8 +18,8 @@ public class VideoRepository {
 
             for (int i = 0; i < 5; i++) {
                 videoList.add(new VideoBean(
-                        "重庆洪崖洞夜景，太美了！ #旅行",
-                        "旅行博主小王",
+                        "深夜治愈：点燃一支香薰蜡烛，放空自己#氛围感",
+                        "生活美学家",
                         "12.5w",
                         R.drawable.cover_1,
                         R.raw.video_1,
@@ -27,8 +27,8 @@ public class VideoRepository {
                 ));
 
                 videoList.add(new VideoBean(
-                        "家里的小猫咪成精了，居然会开门",
-                        "萌宠日记",
+                        "把车窗摇下来，风里都是自由的味道#公路旅行",
+                        "公路日记",
                         "8900",
                         R.drawable.cover_2,
                         R.raw.video_2,
@@ -36,8 +36,8 @@ public class VideoRepository {
                 ));
 
                 videoList.add(new VideoBean(
-                        "由简入繁，十分钟学会这道菜",
-                        "美食杰",
+                        "这片橘子海送给你，想去海边了嘛？",
+                        "海岛听风",
                         "3.4w",
                         R.drawable.cover_3,
                         R.raw.video_3,
@@ -45,8 +45,8 @@ public class VideoRepository {
                 ));
 
                 videoList.add(new VideoBean(
-                        "沉浸式体验雨后的古镇，太治愈了",
-                        "远方日记",
+                        "家里的猫半夜偷偷练琴，这难道是莫扎特转世？",
+                        "喵星人观察员",
                         "2.1w",
                         R.drawable.cover_4,
                         R.raw.video_4,
@@ -54,8 +54,8 @@ public class VideoRepository {
                 ));
 
                 videoList.add(new VideoBean(
-                        "程序员的桌面大揭秘，你的键盘发光吗？",
-                        "极客阿Code",
+                        "今年的第一场雪，大家来一起看雪",
+                        "北方的冬",
                         "5.3w",
                         R.drawable.cover_5,
                         R.raw.video_5,
